65d70a64e37f36ee8d10c4dc1576acd1d6c928e3
kozic009 <kozic009@umn.edu>
2024-02-04 18:15:25 -0600

    feature: tutorial #12 - data gen

---
+++ src/main/java/com/tora/tutorialmod/datagen/ModItemModelProvider.java
@@ -0,0 +1,32 @@
+package com.tora.tutorialmod.datagen;
+
+import com.tora.tutorialmod.TutorialMod;
+import com.tora.tutorialmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, TutorialMod.MODID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        simpleItem(ModItems.SAPPHIRE);
+        simpleItem(ModItems.RAW_SAPPHIRE);
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.PINE_CONE);
+        simpleItem(ModItems.STRAWBERRY);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(TutorialMod.MODID, "item/" + item.getId().getPath()));
+    }
+}

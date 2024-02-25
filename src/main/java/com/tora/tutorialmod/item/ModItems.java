76290d8d3a8cc30643eda4f2ff9f604b7cff07a9
kozic009 <kozic009@umn.edu>
2024-02-25 11:16:40 -0600

    feature: tutorial #17 full armor effects

--- src/main/java/com/tora/tutorialmod/item/ModItems.java
+++ src/main/java/com/tora/tutorialmod/item/ModItems.java
@@ -3,6 +3,7 @@ package com.tora.tutorialmod.item;
 import com.tora.tutorialmod.TutorialMod;
 import com.tora.tutorialmod.item.custom.FuelItem;
 import com.tora.tutorialmod.item.custom.MetalDetectorItem;
+import com.tora.tutorialmod.item.custom.ModArmorItem;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -43,7 +44,7 @@ public class ModItems {
             () -> new ShovelItem(ModToolTiers.SAPPHIRE, 0, 0, new Item.Properties()));
 
     public static final RegistryObject<Item> SAPPHIRE_HELMET = ITEMS.register("sapphire_helmet",
-            () -> new ArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new ModArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> SAPPHIRE_CHESTPLATE = ITEMS.register("sapphire_chestplate",
             () -> new ArmorItem(ModArmorMaterials.SAPPHIRE, ArmorItem.Type.CHESTPLATE, new Item.Properties()));
     public static final RegistryObject<Item> SAPPHIRE_LEGGINGS = ITEMS.register("sapphire_leggings",
